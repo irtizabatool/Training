031966922a9c6108ff07a222748532e90bedc531
irtizabatool <irtizabatool@gmail.com>
2021-01-29 15:49:06 +0530

    Added Ternary statement

--- ifElse.js
+++ ifElse.js
@@ -12,9 +12,13 @@ console.log("Using if..else if...");
 if (number > 0) {
   console.log("The number is positive");
 }
-else if (number == 0) {
+else if (number === 0) {
   console.log("The number is 0");
 }
 else {
   console.log("The number is negative");
 }
+//Using Ternary operator
+console.log("Using ternary operator");
+let result = (number>0) ? "The number is positive" : "The number is either negative or 0";
+console.log(result);
