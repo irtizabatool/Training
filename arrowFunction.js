c511afcba59e8c7325abf5011015e4ccef745aa7
irtizabatool <irtizabatool@gmail.com>
2021-01-29 14:22:19 +0530

    arrow function

---
+++ arrowFunction.js
@@ -0,0 +1,8 @@
+const calculateSquares = (num) => {
+    result = num ** 3;
+    return result;
+ };
+ let numbers = [1,2,3,4];
+ console.log(numbers.map(calculateSquares));
+ let numbers2 = [5,6,7,8];
+ console.log(numbers2.map(calculateSquares));
