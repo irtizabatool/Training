842280d07a23ae8e84455e0d6d8b88b7651ad1ec
irtizabatool <irtizabatool@gmail.com>
2021-01-29 15:36:11 +0530

    if else statements

---
+++ ifElse.js
@@ -0,0 +1,20 @@
+const number = 0;
+//Using if..else
+console.log("Using if..else...");
+if (number>0) {
+  console.log("The number is positive");
+}
+else {
+  console.log("The number is either negative or 0");
+}
+//Using if..else if
+console.log("Using if..else if...");
+if (number > 0) {
+  console.log("The number is positive");
+}
+else if (number == 0) {
+  console.log("The number is 0");
+}
+else {
+  console.log("The number is negative");
+}
