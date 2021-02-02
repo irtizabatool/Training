1062bfa9f2b92ed7d9891b0c0d8c6d12f653057e
irtizabatool <irtizabatool@gmail.com>
2021-02-03 01:15:35 +0530

    Resolving a promise

--- asyncAwait.js
+++ asyncAwait.js
@@ -1,10 +1,10 @@
 function add(n1, n2) {
     return new Promise((resolve, reject) => {
-        setTimeout(() => reject(new Error('Unable to add two numbers')), 2000);
+        setTimeout(() => resolve(n1 + n2), 2000);
     });
 }
 async function asyncSumAndMultiply(num1, num2, num3) {
-    console.log(`The Three numbers are: ${ num1 }, ${ num3 }, ${ num3 }`);
+    console.log(`The Three numbers are: ${ num1 }, ${ num2 }, ${ num3 }`);
     let sum;
     try{
         sum = await add(num1, num2);
@@ -17,4 +17,4 @@ async function asyncSumAndMultiply(num1, num2, num3) {
     console.log(`Product of the sum of first two numbers and the third number is: ${ product }`);
     return product;
 }
-asyncSumAndMultiply(1, 2, 3);
+asyncSumAndMultiply(-2, 1, 3);
