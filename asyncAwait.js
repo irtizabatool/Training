129401bf308a306271c9418c5e5c4dc9cd6d9bbe
irtizabatool <irtizabatool@gmail.com>
2021-02-03 01:02:59 +0530

    Handled error in async/await

--- asyncAwait.js
+++ asyncAwait.js
@@ -3,9 +3,16 @@ function sum(n1, n2) {
 }
 async function asyncSumAndMultiply(num1, num2, num3) {
     console.log(`The Three numbers are: ${ num1 }, ${ num3 }, ${ num3 }`);
-    let sum = await sum(num1, num2);
+    let sum;
+    try{
+        sum = await sum(num1, num2);
+    } catch(err){
+        console.log(`Error: ${e.message}`);
+        sum = 0;
+    }
     let product = num3 * sum;
     console.log(`Sum of First two numbers is: ${ sum }`);
     console.log(`Product of the sum of first two numbers and the third number is: ${ product }`);
+    return product;
 }
 asyncSumAndMultiply(1, 2, 3);
