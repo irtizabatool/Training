5baba0e3c36a3425834b548a13441aaa879007ee
irtizabatool <irtizabatool@gmail.com>
2021-02-03 10:32:29 +0530

    Added objects using constructors

--- objects.js
+++ objects.js
@@ -1,10 +1,11 @@
-const car = {
-    color : 'black',
-    type : 'minivan',
-    registered : 'true',
-    capacity : '7',
-    displayInfo : function() {
-        console.log(`The car is of color ${ car.color }, type: ${ car.type } and has capacity of ${ car.capacity } people`);
-    }
+function Car (type, color, capacity) {
+   this.type = type;
+   this.color = color;
+   this.capacity = capacity;
 }
-car.displayInfo();
+let car1 = new Car('Minivan', 'red', 7);
+let car2 = new Car('Cabrio', 'black', 2);
+console.log(car1.type);
+console.log(car1.capacity);
+console.log(car2.type);
+console.log(car2.capacity);
