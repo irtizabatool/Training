2e74a388cf3217565b3c1a9f94785d70fee99efa
irtizabatool <irtizabatool@gmail.com>
2021-02-04 13:02:30 +0530

    Comparing frequency of 2 characters in string

---
+++ strCount.js
@@ -0,0 +1,13 @@
+function XO(str) {
+    //code here
+  const strArray = Array.from(str);
+  let newStrX = strArray.filter((str1) => (str1.toLowerCase() == 'x'));
+  let newStrO = strArray.filter((str1) => (str1.toLowerCase() == 'o'));
+  let result = (newStrX.length == newStrO.length)? true: false;
+  return result;
+}
+console.log(XO('xo'));
+console.log(XO('xxOo'));
+console.log(XO('xxxm'));
+console.log(XO('Oo'));
+console.log(XO('ooom'));
