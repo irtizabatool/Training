eb7126d1e7f6795638157ec29d1474f84823df95
irtizabatool <irtizabatool@gmail.com>
2021-02-04 11:06:50 +0530

    Solved Promise task and Arrow functions

--- arrowFunction.js
+++ arrowFunction.js
@@ -1,8 +1,27 @@
-const calculateSquares = (num) => {
-    result = num ** 3;
-    return result;
- };
+const calculateSquares = num => num ** 2;
  let numbers = [1,2,3,4];
  console.log(numbers.map(calculateSquares));
  let numbers2 = [5,6,7,8];
  console.log(numbers2.map(calculateSquares));
+
+ // Arrow functions using this keyword
+console.log("Example: Arrow function using THIS keyword");
+class  Person {
+    constructor(name) {
+        this.name = name;
+    }
+    printNameArrow() {
+        setTimeout(() => {
+            console.log('Arrow: ' + this.name);
+        }, 100)
+    }
+    printNameFunction() {
+        setTimeout(function() {
+            console.log('Function: ' + this.name);
+        }, 100)
+    }
+}
+
+let person = new Person('Bob');
+person.printNameArrow();
+person.printNameFunction();
--- promiseTest.js
+++ promiseTest.js
@@ -1,8 +1,3 @@
-const p4 = new Promise((resolve, reject) => {
-    setTimeout(() => resolve('Fourth Promise fulfilled at the end '), 3000);
-}).then((message) => {
-    console.log(message);
-})
 const p1 = new Promise((resolve, reject) => {
     setTimeout(() => resolve('First Promise'), 1000);
 })
@@ -10,7 +5,7 @@ const p2 = new Promise((resolve, reject) => {
     setTimeout(() => resolve('Second Promise'), 1000);
 })
 const p3 = new Promise((resolve, reject) => {
-    setTimeout(() => resolve('Third Promise'), 1000);
+    setTimeout(() => resolve('Third Promise'), 3000);
 })
 Promise.all([
     p1,
@@ -18,4 +13,9 @@ Promise.all([
     p3
 ]).then((messages) => {
     console.log(messages);
+    new Promise((resolve, reject) => {
+        setTimeout(() => resolve('Fourth Promise fulfilled at the end '), 4000);
+    }).then((message) => {
+        console.log(message);
+    })
 })
