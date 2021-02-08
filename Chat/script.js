53d460fe8b67ea1531ffee6f65a4bba590638cb2
irtizabatool <irtizabatool@gmail.com>
2021-02-09 00:39:39 +0530

    Task completed for many to many users

--- Chat/script.js
+++ Chat/script.js
@@ -1,23 +1,29 @@
 let arrMsg = [
-    { user : 0, message: ["hello hello","mike testing hello"]},
-    { user : 1, message: ["1"]},
-    { user : 2, message: ["2"]},
-    { user : 3, message: ["3"]},
-    { user : 4, message: ["4"]}
+    { sender: 1, receiver : 2, message: ["message 1 -2"]},
+    { sender: 1, receiver : 3, message: ["message 1 -3"]},
+    { sender: 1, receiver : 4, message: ["message 1 -4"]},
+    { sender: 1, receiver : 5, message: ["message 1 -5"]},
+    { sender: 2, receiver : 3, message: ["message 2 -3"]},
+    { sender: 2, receiver : 4, message: ["message 2 -4"]},
+    { sender: 2, receiver : 5, message: ["message 2 -5"]},
+    { sender: 3, receiver : 4, message: ["message 3 -4"]},
+    { sender: 3, receiver : 5, message: ["message 3 -5"]},
+    { sender: 4, receiver : 5, message: ["message 4 -5"]}
 ];
 let messages = document.getElementById("messages");
 let textbox = document.getElementById("textbox");
 let button = document.getElementById("button");
 
 button.addEventListener("click", function(){
-    arrMsg[receiver.value].message.push(textbox.value);
-    showMessages(receiver.value);
-    //messages.innerHTML = arrMsg[receiver.value].message;
+	let arrV = getArray(sender.value, reciever.value);
+    arrV.message.push('</br>' + textbox.value);
+    messages.innerHTML = arrV.message;
     textbox.value = "";
 });
-function showMessages(receiver) {
-    let users = arrMsg.find((users) => users.user == receiver);
-	messages.innerHTML = users.message;
-	setTimeout(showMessages, 5000);
+function getArray(sender, receiver){
+	if (sender == receiver){
+		alert("cannot send message to yourself");
+	}
+    let users = arrMsg.find(users =>(users.sender == sender && users.receiver == receiver) || (users.sender == receiver && users.receiver == sender));
+    return users;
 }
-
