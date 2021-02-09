695bbc8713eaefa38ef71bbc8e2e8ef56e652332
irtizabatool <irtizabatool@gmail.com>
2021-02-09 11:04:12 +0530

    Changed the message history on changing the sender and receiver

--- Chat/script.js
+++ Chat/script.js
@@ -1,21 +1,37 @@
 let arrMsg = [
-    { sender: 1, receiver : 2, message: ["message 1 -2"]},
-    { sender: 1, receiver : 3, message: ["message 1 -3"]},
-    { sender: 1, receiver : 4, message: ["message 1 -4"]},
-    { sender: 1, receiver : 5, message: ["message 1 -5"]},
-    { sender: 2, receiver : 3, message: ["message 2 -3"]},
-    { sender: 2, receiver : 4, message: ["message 2 -4"]},
-    { sender: 2, receiver : 5, message: ["message 2 -5"]},
-    { sender: 3, receiver : 4, message: ["message 3 -4"]},
-    { sender: 3, receiver : 5, message: ["message 3 -5"]},
-    { sender: 4, receiver : 5, message: ["message 4 -5"]}
+    { sender: 1, receiver : 2, message: ["messages between user 1 -2"]},
+    { sender: 1, receiver : 3, message: ["messages between user 1 -3"]},
+    { sender: 1, receiver : 4, message: ["messages between user 1 -4"]},
+    { sender: 1, receiver : 5, message: ["messages between user 1 -5"]},
+    { sender: 2, receiver : 3, message: ["messages between user 2 -3"]},
+    { sender: 2, receiver : 4, message: ["messages between user 2 -4"]},
+    { sender: 2, receiver : 5, message: ["messages between user 2 -5"]},
+    { sender: 3, receiver : 4, message: ["messages between user 3 -4"]},
+    { sender: 3, receiver : 5, message: ["messages between user 3 -5"]},
+    { sender: 4, receiver : 5, message: ["messages between user 4 -5"]}
 ];
 let messages = document.getElementById("messages");
 let textbox = document.getElementById("textbox");
 let button = document.getElementById("button");
+let sender = document.getElementById("sender");
+let receiver = document.getElementById("receiver");
+
+sender.addEventListener("change", function(){
+    let arrV = getArray(sender.value, receiver.value);
+   // arrV.message.push('</br>' + textbox.value);
+    messages.innerHTML = arrV.message;
+    textbox.value = "";
+});
+
+receiver.addEventListener("change", function(){
+    let arrV = getArray(sender.value, receiver.value);
+   // arrV.message.push('</br>' + textbox.value);
+    messages.innerHTML = arrV.message;
+    textbox.value = "";
+});
 
 button.addEventListener("click", function(){
-	let arrV = getArray(sender.value, reciever.value);
+	let arrV = getArray(sender.value, receiver.value);
     arrV.message.push('</br>' + textbox.value);
     messages.innerHTML = arrV.message;
     textbox.value = "";
