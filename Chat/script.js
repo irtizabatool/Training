b9f4d9cc73743dcda92b5ed86ba60613c6aa4d5a
irtizabatool <irtizabatool@gmail.com>
2021-02-10 12:35:41 +0530

    Task finished

--- Chat/script.js
+++ Chat/script.js
@@ -1,5 +1,5 @@
 let arrMsg = [
-    { sender: 1, receiver : 2, message1: ["messages from user 1 -2"], message2: ["messages from user 2-1"]},
+    { sender: 1, receiver : 2, message1: ["messages from Alice to Bob"], message2: ["messages from Bob to Alice"]},
     { sender: 1, receiver : 3, message1: ["messages from user 1 -3"], message2: ["messages from user 3-1"]},
     { sender: 1, receiver : 4, message1: ["messages from user 1 -4"], message2: ["messages from user 4-1"]},
     { sender: 1, receiver : 5, message1: ["messages from user 1 -5"], message2: ["messages from user 5-1"]},
@@ -12,8 +12,10 @@ let arrMsg = [
 ];
 let sMsg = document.getElementById("sMessages");
 let rMsg = document.getElementById("rMessages");
-let textbox = document.getElementById("textbox");
-let button = document.getElementById("button");
+let textboxS = document.getElementById("textboxS");
+let textboxR = document.getElementById("textboxR");
+let buttonS = document.getElementById("buttonS");
+let buttonR = document.getElementById("buttonR");
 let sender = document.getElementById("sender");
 let receiver = document.getElementById("receiver");
 
@@ -44,24 +46,42 @@ receiver.addEventListener("change", function(){
     textbox.value = "";
 });
 
-button.addEventListener("click", function(){
+buttonS.addEventListener("click", function(){
 	let arrV = getArray(sender.value, receiver.value);
     if(arrV.sender == sender.value && arrV.receiver == receiver.value){
-        arrV.message1.push('</br>' + textbox.value);
-        sMsg.innerHTML = arrV.message1;
+        arrV.message1.push('</br>' + textboxS.value);
+         sMsg.innerHTML = arrV.message1;
         rMsg.innerHTML = arrV.message2;
     } else {
-        arrV.message2.push('</br>' + textbox.value + '</br>');
+        arrV.message2.push('</br>' + textboxS.value + '</br>');
         sMsg.innerHTML = arrV.message2;
         rMsg.innerHTML = arrV.message1;
     }
    // arrV.message.push('</br>' + textbox.value);
    // messages.innerHTML = arrV.message;
-    textbox.value = "";
+    textboxS.value = "";
 });
+
+buttonR.addEventListener("click", function(){
+	let arrV = getArray(sender.value, receiver.value);
+    if(arrV.sender == sender.value && arrV.receiver == receiver.value){
+         arrV.message2.push('</br>' + textboxR.value);
+         sMsg.innerHTML = arrV.message1;
+         rMsg.innerHTML = arrV.message2;
+    } else {
+        arrV.message1.push('</br>' + textboxR.value + '</br>');
+        sMsg.innerHTML = arrV.message2;
+        rMsg.innerHTML = arrV.message1;
+    }
+   // arrV.message.push('</br>' + textbox.value);
+   // messages.innerHTML = arrV.message;
+    textboxR.value = "";
+});
+
 function getArray(sender, receiver){
 	if (sender == receiver){
 		alert("cannot send message to yourself");
+        return;
 	}
     let users = arrMsg.find(users =>(users.sender == sender && users.receiver == receiver) || (users.sender == receiver && users.receiver == sender));
     return users;
