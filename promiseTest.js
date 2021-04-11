b95f5ce9d1c2aa3429911108566280a1dd5ad944
irtizabatool <irtizabatool@gmail.com>
2021-04-11 19:34:30 +0530

    one
    two

--- promiseTest.js
+++ promiseTest.js
@@ -1,11 +1,12 @@
+let a = [1, 2, 3, 4];
 const p1 = new Promise((resolve, reject) => {
-    setTimeout(() => resolve('First Promise'), 1000);
+    setTimeout(() => resolve(a[0]), 1000);
 })
 const p2 = new Promise((resolve, reject) => {
-    setTimeout(() => resolve('Second Promise'), 1000);
+    setTimeout(() => resolve(a[1]), 1000);
 })
 const p3 = new Promise((resolve, reject) => {
-    setTimeout(() => resolve('Third Promise'), 3000);
+    setTimeout(() => resolve(a[2]), 3000);
 })
 Promise.all([
     p1,
@@ -14,7 +15,7 @@ Promise.all([
 ]).then((messages) => {
     console.log(messages);
     new Promise((resolve, reject) => {
-        setTimeout(() => resolve('Fourth Promise fulfilled at the end '), 4000);
+        setTimeout(() => resolve(a[3]), 4000);
     }).then((message) => {
         console.log(message);
     })
